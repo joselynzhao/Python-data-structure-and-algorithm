0483aa45b2eec0c0351b702ef12f13cdef35939a
zhaojing1995 <1326549593@qq.com>
2020-04-29 19:54:12 +0800

    fang

---
+++ data_structure/dictionary/fang.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@AUTHOR:Joselyn Zhao
+@CONTACT:zhaojing17@foxmail.com
+@HOME_PAGE:joselynzhao.top
+@SOFTWERE:PyCharm
+@FILE:fang.py.py
+@TIME:2020/4/29 19:52
+@DES: 获取字典中3个键的值的过程
+'''
+
+dict = {'shuxue':99,'yuwen':99,'yingyu':99}
+print('shuxue:',dict['shuxue'])
+print('yuwen:',dict['yuwen'])
+print('yingyu:',dict['yingyu'])
+
+# 如果没有 键， 则 KeyError
