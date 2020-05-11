a9d7be1e92c494b203d3b7c15560dd43eb4193da
zhaojing1995 <1326549593@qq.com>
2020-05-11 21:02:40 +0800

    迭代

---
+++ algorithm/iteration/iteration.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''
+@AUTHOR:Joselyn Zhao
+@CONTACT:zhaojing17@foxmail.com
+@HOME_PAGE:joselynzhao.top
+@SOFTWERE:PyCharm
+@FILE:iteration.py
+@TIME:2020/5/11 20:26
+@DES:  解决"非线性方程组"的问题
+'''
+
